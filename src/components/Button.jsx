53de7f9bf644ffbc9bb8b0c355a777b1d855c047
Raed <raedrnp10@gmail.com>
2023-07-13 16:52:51 -0400

    fix some errors

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ function Button(props) {
     return (
         <>
             <button className="text-base tracking-wider font-medium bg-transparent border-solid border-2 border-purple-950 text-purple-950 rounded-lg py-2 px-14 transition hover:border-purple-900 hover:bg-purple-900 hover:text-[#fff]">
-                {props}
+                {props.title}
             </button>
         </>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ function Dropdown(props) {
         onClick={() => setIsOpen((prev) => !prev)}
         className="bg-transparent text-white w-auto h-8 flex items-center justify-center rounded-md hover:underline"
       >
-        {props}
+        {props.title}
         {!isOpen ? <IoCaretDownOutline /> : <IoCaretUpOutline />}
       </button>
       {isOpen && (
