20c892f2ff16bf541c4ae1367366dd5e1b43f5ce
Raed <raedrnp10@gmail.com>
2023-07-13 09:46:24 -0400

    Compressed code, dropdowns into navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import DropNav from "./DropNav"
-import imgURL from '../assets/logo.jpeg'
-import { Dropdown } from "./Dropdown";
+import imgURL from '../assets/logo.jpeg';
+import { IoCaretDownOutline, IoCaretUpOutline, IoMenu, IoClose } from "react-icons/io5";
+import categories from "../constants/categories";
+import { useState } from "react";
 
 function Navbar() {
   return (
@@ -31,6 +32,58 @@ function Navbar() {
         <DropNav />
       </nav>
     </header>
+  );
+}
+
+function Dropdown(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative items-center flex flex-col">
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="bg-transparent text-white w-auto h-8 flex items-center justify-center rounded-md hover:underline"
+      >
+        {props.title}
+        {!isOpen ? <IoCaretDownOutline /> : <IoCaretUpOutline />}
+      </button>
+      {isOpen && (
+        <div className="absolute w-screen sm:w-auto h-auto top-9 bg-purple-900 flex flex-col items-center rounded-sm">
+          {categories.map((item, i) => (
+            <ul>
+              <li className="py-3 px-10" key={i}>
+                <a href={`${item.path}`}>{item.title}</a>
+              </li>
+            </ul>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+function DropNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        {!isOpen ? <IoMenu /> : <IoClose />}
+      </button>
+        {isOpen && (
+          <div className="absolute w-full h-64 top-16 right-0 bg-purple-900 flex flex-col items-center justify-around rounded-sm shadow-2xl">
+            <a href="#" className="hover:underline">
+              About me
+            </a>
+            <a href="#" className="hover:underline">
+              Contact
+            </a>
+            <div>
+              <Dropdown title="Projects" />
+            </div>
+          </div>
+        )}
+    </>
   );
 }
 
